bf5844ca035e2c589669c6a6a721bb0b3f965a7c
yusong wang <admin@wangyusong.com>
2023-06-03 11:04:50 +0800

    add tokens

--- crates/sui-indexer/src/indexer/mod.rs
+++ crates/sui-indexer/src/indexer/mod.rs
@@ -3,10 +3,8 @@ pub mod receiver;
 use anyhow::{Error, Result};
 use diesel::pg::PgConnection;
 use diesel::r2d2::{ConnectionManager, Pool};
-use diesel::ExpressionMethods;
-use diesel::QueryDsl;
-use diesel::RunQueryDsl;
 use futures::future::join_all;
+use std::sync::Arc;
 
 use futures::StreamExt;
 use redis::Commands;
@@ -16,7 +14,9 @@ use sui_sdk::types::messages_checkpoint::CheckpointSequenceNumber;
 use sui_sdk::SuiClient;
 use tokio::sync::mpsc::Sender;
 
-use crate::models::activities::batch_insert as batch_insert_activities;
+use crate::models::activities::{
+    batch_insert as batch_insert_activities, Activity,
+};
 use crate::models::check_point::query_check_point;
 use crate::{
     fetch_changed_objects, get_object_changes, multi_get_full_transactions,
@@ -30,10 +30,12 @@ use sui_sdk::rpc_types::{
 use crate::config::Config;
 use crate::handlers::activity::parse_tokens_activity;
 use crate::handlers::bobyard_event_catch::{
-    event_handle, parse_bob_yard_event,
+    event_handle, parse_bob_yard_event, BobYardEvent,
 };
-use crate::handlers::collection::{collection_indexer_work, parse_collection};
-use crate::handlers::token::{parse_tokens, token_indexer_work};
+use crate::handlers::collection::{
+    collection_indexer_work, parse_collection, Collection,
+};
+use crate::handlers::token::{parse_tokens, token_indexer_work, Token};
 use crate::indexer::receiver::IndexingMessage;
 use tracing::{debug, info, warn};
 
@@ -42,6 +44,8 @@ extern crate redis;
 use crate::schema::check_point::dsl::check_point;
 use crate::schema::check_point::{chain_id, version};
 use crate::MULTI_GET_CHUNK_SIZE;
+
+const DOWNLOAD_RETRY_INTERVAL_IN_SECS: u64 = 10;
 
 #[derive(Clone)]
 pub(crate) struct Indexer {
@@ -86,11 +90,6 @@ impl Indexer {
 
         let batch_index = self.config.batch_index;
 
-        // info!(
-        //     "start indexer worker the last sequence number: {}",
-        //     last_sequence
-        // );
-
         loop {
             let download_futures = (indexer..(indexer + batch_index))
                 .map(|x| download_checkpoint_data(&self.sui_client, x));
@@ -117,6 +116,11 @@ impl Indexer {
                     "No checkpoints were downloaded for sequence number {}, retrying...",
                     indexer
                 );
+                tokio::time::sleep(std::time::Duration::from_secs(
+                    DOWNLOAD_RETRY_INTERVAL_IN_SECS,
+                ))
+                .await;
+
                 continue;
             }
 
@@ -132,6 +136,7 @@ impl Indexer {
             //         .get_result::<(i64, i64)>(&mut pg);
             //
             // assert_eq!(Ok((1, indexer as i64)), updated_row);
+
             let last_sequence = self
                 .sui_client
                 .read_api()
@@ -151,84 +156,23 @@ impl Indexer {
         let mut receiver = self.check_point_data_receiver.clone().into_stream();
         let mut pg = self.postgres.get()?;
         let mut redis = self.redis.get_connection()?;
-        let mut collects_set: HashMap<String, String> =
-            redis.hgetall("collections")?;
+        let mut collects_set: Arc<HashMap<String, String>> =
+            Arc::new(redis.hgetall("collections")?);
+
+        let bob_yard_contract = self.config.bob_yard.clone();
 
         while let Some(downloaded_checkpoints) = receiver.next().await {
-            for (check_point_data, _, object_changed, events) in
-                downloaded_checkpoints
-            {
-                let collections = parse_collection(
-                    &object_changed,
-                    &mut redis,
-                    &mut collects_set,
-                )?;
-
-                let tokens = parse_tokens(&object_changed, &mut collects_set)?;
-                let bob_yard_events =
-                    parse_bob_yard_event(&events, &self.config.bob_yard)?;
-                let token_activities =
-                    parse_tokens_activity(&bob_yard_events, &tokens);
-
-                for (msg, collection) in collections.iter() {
-                    self.sender
-                        .send(IndexingMessage::Collection((
-                            (*msg).into(),
-                            collection.clone(),
-                        )))
-                        .await?;
-                }
-
-                for (msg, t) in tokens.iter() {
-                    self.sender
-                        .send(IndexingMessage::Token((
-                            (*msg).into(),
-                            t.0.clone(),
-                        )))
-                        .await?;
-                }
-
-                pg.build_transaction().read_write().run(|conn| {
-                    if collections.len() > 0 {
-                        collection_indexer_work(&collections, conn).unwrap();
-                    }
-
-                    if tokens.len() > 0 {
-                        token_indexer_work(&tokens, conn).unwrap();
-                    }
-
-                    if bob_yard_events.len() > 0 {
-                        event_handle(
-                            &bob_yard_events,
-                            check_point_data.timestamp_ms as i64,
-                            conn,
-                        )
-                        .unwrap();
-                    }
-
-                    if token_activities.len() > 0 {
-                        batch_insert_activities(conn, &token_activities)
-                            .unwrap();
-                    }
-
-                    let updated_row = diesel::update(
-                        check_point.filter(chain_id.eq(1)),
+            let parse_all_futures: Vec<_> = downloaded_checkpoints
+                .into_iter()
+                .map(|data| {
+                    parse_all(
+                        &bob_yard_contract,
+                        redis,
+                        collects_set.clone(),
+                        data,
                     )
-                    .set(version.eq(check_point_data.sequence_number as i64))
-                    .get_result::<(i64, i64)>(conn);
-                    assert_eq!(
-                        Ok((1, check_point_data.sequence_number as i64)),
-                        updated_row
-                    );
-
-                    info!(
-                        sequence_number = check_point_data.sequence_number,
-                        "indexer store success processed"
-                    );
-
-                    Ok::<(), anyhow::Error>(())
-                })?;
-            }
+                })
+                .collect();
         }
 
         Ok(())
@@ -244,6 +188,88 @@ impl Indexer {
     // }
 }
 
+async fn parse_all(
+    bob_yard_contract: &String, rds: &mut redis::Connection,
+    collects_set: Arc<HashMap<String, String>>,
+    (check_point_datas, transactions, objects, events): CheckpointData,
+) -> Result<(
+    Vec<(ObjectStatus, Collection)>,
+    Vec<(ObjectStatus, (Token, std::string::String))>,
+    Vec<BobYardEvent>,
+    Vec<Activity>,
+)> {
+    // for (check_point_data, _, object_changed, events) in
+    //     downloaded_checkpoints
+    // {
+
+    let collections = parse_collection(&objects, rds, &mut collects_set)?;
+    let tokens = parse_tokens(&objects, &mut collects_set)?;
+    let bob_yard_events = parse_bob_yard_event(&events, &bob_yard_contract)?;
+    let token_activities = parse_tokens_activity(&bob_yard_events, &tokens);
+
+    Ok::<
+        (
+            Vec<(ObjectStatus, Collection)>,
+            Vec<(ObjectStatus, (Token, std::string::String))>,
+            Vec<BobYardEvent>,
+            Vec<Activity>,
+        ),
+        anyhow::Error,
+    >((collections, tokens, bob_yard_events, token_activities))
+    // for (msg, collection) in collections.iter() {
+    //     sender
+    //         .send(IndexingMessage::Collection((
+    //             (*msg).into(),
+    //             collection.clone(),
+    //         )))
+    //         .await?;
+    // }
+
+    // for (msg, t) in tokens.iter() {
+    //     self.sender
+    //         .send(IndexingMessage::Token(((*msg).into(), t.0.clone())))
+    //         .await?;
+    // }
+
+    // pg.build_transaction().read_write().run(|conn| {
+    //     if collections.len() > 0 {
+    //         collection_indexer_work(&collections, conn).unwrap();
+    //     }
+
+    //     if tokens.len() > 0 {
+    //         token_indexer_work(&tokens, conn).unwrap();
+    //     }
+
+    //     if bob_yard_events.len() > 0 {
+    //         event_handle(
+    //             &bob_yard_events,
+    //             check_point_data.timestamp_ms as i64,
+    //             conn,
+    //         )
+    //         .unwrap();
+    //     }
+
+    //     if token_activities.len() > 0 {
+    //         batch_insert_activities(conn, &token_activities).unwrap();
+    //     }
+
+    //     let updated_row = diesel::update(check_point.filter(chain_id.eq(1)))
+    //         .set(version.eq(check_point_data.sequence_number as i64))
+    //         .get_result::<(i64, i64)>(conn);
+    //     assert_eq!(
+    //         Ok((1, check_point_data.sequence_number as i64)),
+    //         updated_row
+    //     );
+
+    //     info!(
+    //         sequence_number = check_point_data.sequence_number,
+    //         "indexer store success processed"
+    //     );
+
+    //     Ok::<(), anyhow::Error>(())
+    // })?;
+    //}
+}
 async fn download_checkpoint_data(
     sui_client: &SuiClient, seq: CheckpointSequenceNumber,
 ) -> Result<(
